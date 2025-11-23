14f8b69766dab337a698fb20c5ac0321714e48ef
sqwidwhat <andikamelawi.net11@gmail.com>
2025-11-24 06:35:56 +0700

    Tambah data

--- src/app/mahasiswa/mahasiswa.ts
+++ src/app/mahasiswa/mahasiswa.ts
@@ -20,14 +20,14 @@ export class Mahasiswa implements AfterViewInit {
 
   constructor(private httpClient: HttpClient, private renderer: Renderer2) {}
   ngAfterViewInit(): void {
+    this.renderer.removeClass(document.body, 'sidebar-open');
+    this.renderer.addClass(document.body, 'sidebar-closed');
+    this.renderer.addClass(document.body, 'sidebar-collapse');
     this.table1 = $('#table1').DataTable({
       responsive: true,
       autoWidth: false,
     });
     this.bindMahasiswa();
-    this.renderer.removeClass(document.body, 'sidebar-open');
-    this.renderer.addClass(document.body, 'sidebar-closed');
-    this.renderer.addClass(document.body, 'sidebar-collapse');
   }
   bindMahasiswa(): void {
     this.httpClient
@@ -64,5 +64,71 @@ export class Mahasiswa implements AfterViewInit {
 
         this.table1.draw(false);
       });
+  }
+  showTambahModal(): void {
+    $('#tambahModal').modal();
+  }
+  postRecord(): void {
+    var alamat = $('#alamatText').val();
+    var jenisKelamin = $('#jenisKelaminSelect').val();
+    var jp = $('#jpSelect').val();
+    var nama = $('#namaText').val();
+    var nim = $('#nimText').val();
+    var statusNikah = $('#statusNikahSelect').val();
+    var tahunMasuk = $('#tahunMasukText').val();
+    var tanggalLahir = $('#tanggalLahirText').val();
+    var tempatLahir = $('#tempatLahirText').val();
+
+    if (nim.length == 0) {
+      alert('NIM belum diisi');
+      return;
+    }
+
+    if (nama.length == 0) {
+      alert('Nama belum diisi');
+      return;
+    }
+
+    if (tempatLahir.length == 0) {
+      alert('Tempat lahir belum diisi');
+      return;
+    }
+    alamat = encodeURIComponent(alamat);
+    jenisKelamin = encodeURIComponent(jenisKelamin);
+    jp = encodeURIComponent(jp);
+    nama = encodeURIComponent(nama);
+    nim = encodeURIComponent(nim);
+    statusNikah = encodeURIComponent(statusNikah);
+    tahunMasuk = encodeURIComponent(tahunMasuk);
+    tanggalLahir = encodeURIComponent(tanggalLahir);
+    tempatLahir = encodeURIComponent(tempatLahir);
+    var url =
+      'https://stmikpontianak.cloud/011100862/tambahMahasiswa.php' +
+      '?alamat=' +
+      alamat +
+      '&jenisKelamin=' +
+      jenisKelamin +
+      '&jp=' +
+      jp +
+      '&nama=' +
+      nama +
+      '&nim=' +
+      nim +
+      '&statusPernikahan=' +
+      statusNikah +
+      '&tahunMasuk=' +
+      tahunMasuk +
+      '&tanggalLahir=' +
+      tanggalLahir +
+      '&tempatLahir=' +
+      tempatLahir;
+
+    this.httpClient.get(url).subscribe((data: any) => {
+      console.log(data);
+      alert(data.status + ' --> ' + data.message);
+
+      this.bindMahasiswa();
+      $('#tambahModal').modal('hide');
+    });
   }
 }
